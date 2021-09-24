7c7ad09ed64408d82183527a0a09523305c5d657
German Arcila <german@KCJ-MB-garcila01.local>
2021-09-24 08:30:32 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	let newTodo = "";
+	let todos = ["a", "b"];
+
+	function addTodo() {
+		todos = [...todos, newTodo];
+		newTodo = "";
+	}
+</script>
+
+<main>
+	<h1>TODO</h1>
+	<form id="todoForm" on:submit|preventDefault={addTodo}>
+		<input
+			type="text"
+			name="new-todo"
+			bind:value={newTodo}
+			placeholder="Add New Todo"
+		/>
+	</form>
+	<ul>
+		{#each todos as todo}
+			<li>{todo}</li>
+		{/each}
+	</ul>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ wishes.md
@@ -0,0 +1,15 @@
+Code split.
+Form in new file
+
+Features
+Update by clicking on the todo itself
+Delete by clicking on an icon on the left or right
+Mark as completed but leave in place
+Mark as completed and move to completed todos
+
+Add CSS fanciness to mark todos as completed
+
+Save to  localStorage
+Save to database
+GraphQL
+Authentication Login/Sign up
